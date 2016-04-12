3674f3c259ebb68fd2ebc74a0d3db052f37a0fd5
Jazmin90 <jazmin.bansagi@gmail.com>
2016-04-12 19:26:59 -0400

    add files CacheFunction

--- CacheFunction.R
+++ CacheFunction.R
@@ -1,3 +1,9 @@
+## This overall function computed the inverse of a matrix
+## If the inverse has already been calculated, it gets the inverse
+## From the cache
+
+## Here we are writing a function that will calculate the inverse of the function
+
 makeCacheMatrix <- function (x = matrix ()) {
   inv <- NULL
   set <- function(y){
@@ -10,14 +16,19 @@ makeCacheMatrix <- function (x = matrix ()) {
   list (set=set, get=get, sitinv=setinv, getinv=getinv)
 }
 
+## If the inverse is null, means the inverse has already been calculated in the cache
+## So the first part of the function retrieves this
 cacheMatrix <- function (x, ...) {
   inv <- x$getinv()
   if(!is.null(inv)){
     message("getting cached data")
     return(inv)
   }
+## If the inverse has not already been calculated, solve the inverse from the data
   data <- x$get()
   inv <- solve(data,...)
   x$setmean(inv)
   inv
 }
+
+## Returns a matrix that is the inverse of X
